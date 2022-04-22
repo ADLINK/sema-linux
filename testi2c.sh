dfd3cb60ac043d288b4345919b6e543f02b75ae3
Ryan ZJ Huang <ryanzj.huang@adlinktech.com>
2022-04-22 15:06:05 +0800

    remove the unnecessary items

--- testi2c.sh
+++
@@ -1,5 +0,0 @@
-sudo ./semautil /i2c write_xfer 1 a8 2 00 4 aa 11 22 33
-sudo ./semautil /i2c read_xfer 1 a8 2 00 4
-sudo ./semautil /i2c write_xfer 1 c4 1 3 40 2 90
-sudo ./semautil /i2c read_xfer 1 c4 1 3
-sudo ./semautil /i2c read_raw 1 c4 3
