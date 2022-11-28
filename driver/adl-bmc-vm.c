8543650659f933ea4589d6b9804a6c4e843a9f66
adlinkccoe <viswanathan@adlinktech.com>
2022-11-28 18:00:51 +0530

    Added support for Ubuntu 22.04 SEMA build Voltage Monitoring

--- driver/adl-bmc-vm.c
+++ driver/adl-bmc-vm.c
@@ -18,6 +18,7 @@
 #include <linux/string.h>
 #include <linux/sysfs.h>
 #include <linux/device.h>
+#include <linux/delay.h>
 
 
 #include "adl-bmc.h"
@@ -30,6 +31,7 @@ struct adl_bmc_vm_data *vm_data;
 struct adl_bmc_vm_data {
 	struct regulator_desc adl_bmc_vm_desc[16];
 	struct regulator_dev *adl_bmc_vm_rdev[16];
+	struct regulator *adl_bmc_vm_dev[16];
 	struct adl_bmc_dev *adl_dev;
 	char name_arr[16][256];
 	char adlink_name[16][256];
@@ -37,8 +39,8 @@ struct adl_bmc_vm_data {
 
 };
 
-static int voltagenum=0;
-unsigned int voltagevalue[32];
+static int voltagenum = 0;
+unsigned int voltagevalue[32] = {0};
 
 int converttoint(char *buf)
 {
@@ -55,9 +57,7 @@ int converttoint(char *buf)
 static int adl_bmc_vm_list_voltage(struct regulator_dev *rdev, unsigned selector)
 {
 
-	printk("func: %s\n", __func__);
-
-
+	debug_printk("func: %s\n", __func__);
 	return 0;
 }
 
@@ -91,6 +91,7 @@ static int adl_bmc_vm_get_voltage(struct regulator_dev *rdev)
 	if(ret != 2)
 		return -EINVAL;
 
+	msleep(30);
 	/*read the gain value*/
         ret = adl_bmc_i2c_read_device(vm_data->adl_dev, ADL_BMC_CMD_GET_ADC_SCALE, 32, buff_gain);
 	if (ret < 0)
@@ -118,11 +119,44 @@ static struct regulator_ops adl_bmc_vm_ops = {
 
 static ssize_t sysfs_show_voltage_log(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
 {
-        int temp;
-        temp  = voltagenum;
-        voltagenum = temp + voltagenum;
-        return sprintf(buf,"%u\n",voltagevalue[voltagenum]);
-         
+	int ret = 0;
+	unsigned char buff_hm[32] = {0};
+	unsigned char buff_gain[32] = {0};
+	unsigned char cmd_hm;
+	u64 vol_val, vol_val_fl;
+	unsigned short gain;
+
+	debug_printk("%s id: %d \n", __FUNCTION__, voltagenum);
+	
+	if (voltagenum < 8) {
+		cmd_hm = ADL_BMC_CMD_RD_AIN0 + voltagenum;
+	}
+	else if (voltagenum >= 8 && voltagenum < 16) {
+		cmd_hm = ADL_BMC_CMD_RD_AIN8 + (voltagenum - 8);
+	}
+	else
+		return -EINVAL;
+
+	debug_printk("I2C commmand: 0x%x\n", cmd_hm);
+
+	
+	/*read the hwmon register for voltage*/
+        ret = adl_bmc_i2c_read_device(vm_data->adl_dev, cmd_hm, 2, buff_hm);
+	if(ret != 2)
+		return -EINVAL;
+
+	msleep(30);
+	/*read the gain value*/
+        ret = adl_bmc_i2c_read_device(vm_data->adl_dev, ADL_BMC_CMD_GET_ADC_SCALE, 32, buff_gain);
+	if (ret < 0)
+		return ret;
+	gain = (buff_gain[voltagenum * 2] << 8) | buff_gain[(voltagenum * 2) + 1];
+	vol_val_fl = 3223 * gain;
+	vol_val = vol_val_fl * (buff_hm[0] << 8 | buff_hm[1]);
+	vol_val = vol_val / 1000000;
+
+	return sprintf(buf,"%llu\n", vol_val);
+        
 }
 
 static ssize_t sysfs_store_voltage_log(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
@@ -133,7 +167,11 @@ static ssize_t sysfs_store_voltage_log(struct kobject *kobj, struct kobj_attribu
 
 static ssize_t sysfs_show_voltage_info(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
 {
-        return sprintf(buf,"%s\n",vm_data->adl_bmc_vm_desc[voltagenum].name);
+
+	if (voltagenum >= vm_data->cnt) 
+		return -EINVAL;
+
+	return sprintf(buf,"%s\n",vm_data->adl_bmc_vm_desc[voltagenum].name);
 }
 
 static ssize_t sysfs_store_voltage_info(struct kobject *kobj, struct kobj_attribute *attr, const char *buf,size_t count)
@@ -205,18 +243,16 @@ static int adl_bmc_vm_probe(struct platform_device *pdev)
 		ret = adl_bmc_i2c_write_device(vm_data->adl_dev, ADL_BMC_CMD_EXT_HW_DESC, 1, data);
 		if (ret < 0)
 			debug_printk("return failed  in write i=%d\n", i);
+		msleep(30);
 		ret = adl_bmc_i2c_read_device(vm_data->adl_dev, ADL_BMC_CMD_EXT_HW_DESC, 16, data);
 		if (ret < 0)
 			debug_printk("return failed in read i=%d\n", i);
-		printk("Buffer is %s\n", data +4);
+		//printk("Buffer is %s\n", data +4);
 
 
 		data[15] = 0;
 		strcat(vm_data->name_arr[i], data + 4);
-
 		vm_data->adl_bmc_vm_desc[i].name = vm_data->name_arr[i];
-		//vm_data->adl_bmc_vm_desc[i].supply_name = vm_data->adlink_name[i];  
-              
 		vm_data->adl_bmc_vm_desc[i].id = i;
 		vm_data->adl_bmc_vm_desc[i].type = REGULATOR_VOLTAGE;
 		vm_data->adl_bmc_vm_desc[i].ops = &adl_bmc_vm_ops;
@@ -246,7 +282,7 @@ static int adl_bmc_vm_probe(struct platform_device *pdev)
 
 ret_failed:
 	for(j = 0; j < i; j++) {
-		devm_regulator_unregister(&pdev->dev, vm_data->adl_bmc_vm_rdev[j]);
+		devm_regulator_put(vm_data->adl_bmc_vm_dev[j]);
 		debug_printk("In for loop\n");
 	}
 
@@ -270,11 +306,10 @@ static int adl_bmc_vm_remove(struct platform_device *pdev)
 	kobject_put(kobj_ref);
 	for (i = 0; i < vm_data->cnt; i++) 
 	{
-		devm_regulator_unregister(&pdev->dev, vm_data->adl_bmc_vm_rdev[i]);
+		devm_regulator_put(vm_data->adl_bmc_vm_dev[i]);
 	}
 
 	devm_kfree(&pdev->dev, vm_data);
-
 
 	return 0;
 }
